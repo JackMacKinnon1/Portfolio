2f655858e63202ae0f81f231a1a21f1f8c861c65
JackMackinno1 <w0449149@campus.nscc.ca>
2023-02-24 19:53:36 -0400

    Got some simple animations

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
+const transition = document.querySelector('.transition');
+
+// nav bar animation
+const controls = document.querySelector(".controls");
+
+document.addEventListener("mousemove", (event) => {
+    if (event.clientX > window.innerWidth - (sectBtns[0].offsetWidth + sectBtns[0].clientWidth + 5)) {
+        controls.style.right = "0";
+    } else {
+        controls.style.right = "-200px";
+    }
+});
+
+function perform_transition() {
+
+}
 
 function PageTransitions() {
     // Button click active class
@@ -12,6 +28,32 @@ function PageTransitions() {
             this.className += ' active-btn';
         });
     }
+
+    // showing sections
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            transition.style.display = 'block';
+            transition.classList.add('show')
+            setTimeout(() => {
+                // remove selected from other buttons
+                sectBtns.forEach((btn) => {
+                    btn.classList.remove('active')
+                })
+                e.target.classList.add('active')
+
+                // hide other sections
+                sections.forEach((section) => {
+                    section.classList.remove('active')
+                })
+                const element = document.getElementById(id)
+                element.classList.add('active')
+
+                transition.classList.remove('show')
+            }, 1000);
+        }
+    })
 }
+
 
 PageTransitions();
